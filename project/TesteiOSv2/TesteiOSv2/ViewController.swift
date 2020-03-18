a7c720460d0fc11dd731b5d718d3b14ea3aed37a
Gabriel Sanzone <gabriel.sanzone@fcamara.com.br>
2020-03-17 23:46:05 -0300

    init Project

---
+++ project/TesteiOSv2/TesteiOSv2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesteiOSv2
+//
+//  Created by Gabriel Sanzone on 17/03/20.
+//  Copyright © 2020 Gabriel Chirico Mahtuk de Melo Sanzone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
